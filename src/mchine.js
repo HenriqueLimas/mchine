ae36e70f9209950eeb654ccbb0b80a9cf9e47cd5
HenriqueLimas <henrique.ramos.limas@gmail.com>
2018-07-09 01:23:03 +0200

    First version of parallel states

--- src/mchine.js
+++ src/mchine.js
@@ -1,26 +1,41 @@
 // @flow
 
-interface State {
-  [string]: any;
+type State = {
+  [string]: any,
   actions: {
     [string]: (...payload: any) => any
-  };
-}
+  }
+};
 
-interface StateMachine {
-  [string]: State;
-}
+type ParallelState = {
+  [string]: State
+};
+
+type Statechart = State | ParallelState;
+
+type StateMachine = {
+  initial?: string,
+  parallel?: boolean,
+  states: {
+    [string]: State
+  }
+};
 
 type TDispatch = (actionName: string, ...payload: Array<any>) => any;
 
 interface MChine {
   dispatch: TDispatch;
   changeStateTo(string): void;
-  getCurrentState(): StateMachine;
+  getCurrentState(): Statechart;
 }
 
-const mchine = (stateMachine: StateMachine, initialState: ?string): MChine => {
-  let _statesHistory: Array<State> = [];
+const _get = (target, path) =>
+  path.split('.')
+    .reduce((target, key) => target && target[key], target)
+
+const mchine = (stateMachine: StateMachine): MChine => {
+  let _statesHistory: Array<Statechart> = [];
+  const states = stateMachine.states;
 
   const machine = {
     dispatch(actionName: string, ...payload: Array<any>): any {
@@ -28,31 +43,74 @@ const mchine = (stateMachine: StateMachine, initialState: ?string): MChine => {
       const actions = currentState.actions || {};
 
       return Object.keys(actions)
-        .filter((key) => key === actionName)
+        .filter(key => key === actionName)
         .reduce(
           (_, actionName) => actions[actionName](machine, ...payload),
           null
         );
     },
     changeStateTo(stateName: string) {
-      _statesHistory = _statesHistory.concat([
-        Object.assign({__mchine_name__: stateName}, stateMachine[stateName]),
-      ]);
+      _statesHistory = _statesHistory.concat(
+        [stateName]
+          .map(createState(stateMachine))
+          .map(stateOrParallel(machine, stateMachine))
+      );
     },
-    get currentState(): State {
+    get currentState(): Statechart {
       const currentState = _statesHistory[_statesHistory.length - 1];
       return currentState && Object.assign({}, currentState);
     },
     get history(): Array<State> {
-      return _statesHistory.map((state) => Object.assign({}, state));
+      return _statesHistory.map(state => Object.assign({}, state));
     },
+    getCurrentState(): Statechart {
+      return machine.currentState;
+    }
   };
 
-  if (initialState) {
-    machine.changeStateTo(initialState);
-  }
+  initialState(machine, stateMachine)
 
   return machine;
 };
+
+const createState = (stateMachine: StateMachine) => (
+  stateName: string
+): State =>
+  Object.assign(
+    {
+      __mchine_name__: stateName.split('.')[0]
+    },
+    _get(stateMachine.states, stateName)
+  );
+
+const initialState = (
+  machine: MChine,
+  stateMachine: StateMachine
+): Statechart =>
+  stateMachine.parallel
+    ? InitialParallelState(machine, stateMachine)
+    : InitialState(machine, stateMachine);
+
+const InitialState = (machine: MChine, stateMachine: StateMachine, parentState: string): State =>
+  machine.changeStateTo((parentState ? `${parentState}.`: '') + stateMachine.initial);
+
+const InitialParallelState = (
+  machine: MChine,
+  stateMachine: StateMachine
+): ParallelState =>
+  Object.keys(stateMachine.states).reduce(
+    (state: ParallelState, stateName: string): ParallelState =>
+      Object.assign(state, {
+        [stateName]: InitialState(machine, stateMachine.states[stateName], stateName)
+      }),
+    {}
+  );
+
+const stateOrParallel = (machine: MChine, stateMachine: StateMachine) => (
+  state: State
+): Statechart =>
+  stateMachine.parallel
+    ? Object.assign({}, machine.getCurrentState(), { [state.__mchine_name__]: state })
+    : state;
 
 export default mchine;
--- src/mchine.test.js
+++ src/mchine.test.js
@@ -1,73 +1,117 @@
-import mchine from './mchine';
+import mchine from "./mchine";
 
-describe('mchine', () => {
+describe("mchine", () => {
   let stateMachine;
 
   beforeEach(() => {
     stateMachine = {
-      idle: {
-        actions: {
-          send(m) {
-            m.changeStateTo('sending');
-          },
+      initial: 'idle',
+      states: {
+        idle: {
+          actions: {
+            send(m) {
+              m.changeStateTo("sending");
+            }
+          }
         },
-      },
-      sending: {
-        actions: {},
-      },
+        sending: {
+          actions: {}
+        }
+      }
     };
   });
 
-  it('should not set the current state without passing the initial state', () => {
+  it("should set the current state as the initial state", () => {
     const m = mchine(stateMachine);
-
-    expect(m.currentState).toBeUndefined();
-  });
-
-  it('should set the current state as the initial state', () => {
-    const m = mchine(stateMachine, 'idle');
 
     expect(m.currentState).toBeDefined();
   });
 
-  it('should call the action', () => {
+  it("should call the action", () => {
     const mkSend = jest.fn().mockImplementation((m, email, password) => {});
-    stateMachine.idle.actions.send = mkSend;
+    stateMachine.states.idle.actions.send = mkSend;
 
-    const m = mchine(stateMachine, 'idle');
+    stateMachine.initial = "idle";
+    const m = mchine(stateMachine);
 
     expect(mkSend.mock.calls).toHaveLength(0);
 
-    const email = 'john@example.com';
-    const password = 'Some_secure_password42';
+    const email = "john@example.com";
+    const password = "Some_secure_password42";
 
-    m.dispatch('send', email, password);
+    m.dispatch("send", email, password);
 
     expect(mkSend.mock.calls).toHaveLength(1);
     expect(mkSend.mock.calls[0][1]).toBe(email);
     expect(mkSend.mock.calls[0][2]).toBe(password);
   });
 
-  describe('dispatch()', () => {
-    it('should not throw an error when there is no current state', () => {
+  describe("dispatch()", () => {
+    it("should not throw an error when there is no current state", () => {
       const m = mchine(stateMachine);
       expect(() => {
-        m.dispatch('send');
+        m.dispatch("send");
       }).not.toThrow();
     });
   });
 
-  describe('history', () => {
-    it('should return all states', () => {
+  describe("history", () => {
+    it("should return all states", () => {
       const m = mchine(stateMachine);
 
-      expect(m.history).toHaveLength(0);
-
-      m.changeStateTo('idle');
       expect(m.history).toHaveLength(1);
 
-      m.dispatch('send');
+      m.dispatch("send");
       expect(m.history).toHaveLength(2);
+    });
+  });
+
+  describe("ParallelState", () => {
+    beforeEach(() => {
+      stateMachine = {
+        parallel: true,
+        states: {
+          bold: {
+            initial: 'on',
+            states: {
+              on: {
+                actions: {
+                  toggle: m => m.changeStateTo('bold.off')
+                }
+              },
+              off: {
+                actions: {
+                  toggle: m => m.changeStateTo('bold.on')
+                }
+              }
+            }
+          },
+          italic: {
+            initial: 'on',
+            states: {
+              on: {
+                value: 'on',
+                actions: {
+                  toggle: m => m.changeStateTo('italic.off')
+                }
+              },
+              off: {
+                value: 'off',
+                actions: {
+                  toggle: m => m.changeStateTo('italic.on')
+                }
+              }
+            }
+          }
+        }
+      };
+    });
+
+    it("should return start all the states", () => {
+      const m = mchine(stateMachine);
+
+      expect(Object.keys(m.currentState)).toEqual(['bold', 'italic']);
+      console.log(m.currentState)
     });
   });
 });
