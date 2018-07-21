bc1b4968f863335cb325bd58e31c92204ea36d68
HenriqueLimas <henrique.limas@freighthub.com>
2018-07-21 13:15:25 +0200

    Update demo with v1 🕹

--- demo/stateMachine.m.js
+++ demo/stateMachine.m.js
@@ -1,52 +1,29 @@
-const API = {
-  login: () => new Promise((_, r) => setTimeout(r, 2000))
-};
-
-const login = (machine, email, password) => {
-  console.log('sending')
-  machine.changeStateTo('sending')
-  return API.login(email, password)
-    .then(() => machine.dispatch('success'))
-    .catch(() => machine.dispatch('error'));
-
-};
-
 const stateMachine = {
-  idle: {
-    actions: {
-      login
-    }
+  initial: 'idle',
+  states: {
+    idle: {
+      events: {
+        login: {
+          target: 'sending',
+        },
+      },
+    },
+    sending: {
+      events: {
+        success: {
+          target: 'idle',
+        },
+        error: {
+          target: 'error',
+        },
+      },
+    },
+    error: {
+      events: {
+        target: 'sending',
+      },
+    },
   },
-  sending: {
-    components: {
-      email: {
-        disabled: true
-      },
-      password: {
-        disabled: true
-      },
-      login: {
-        disabled: true
-      },
-      loading: true
-    },
-    actions: {
-      success: machine => {
-        machine.changeStateTo('idle');
-      },
-      error: machine => {
-        machine.changeStateTo('error');
-      }
-    }
-  },
-  error: {
-    components: {
-      error: { visible: true }
-    },
-    actions: {
-      login
-    },
-  }
 };
 
 export default stateMachine;
