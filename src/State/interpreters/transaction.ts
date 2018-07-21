0af75405e6de49fec82dcc39f38b13cb6726a0b5
HenriqueLimas <henrique.limas@freighthub.com>
2018-07-21 09:57:21 +0200

    Use same method to create transitions ♻️

--- src/State/interpreters/transaction.ts
+++ src/State/interpreters/transaction.ts
@@ -1,15 +1,16 @@
 import {EventSchema} from '../../Schema/TransitionSchema';
 import {Transition} from '../../Transition';
+import {newTransition} from '../../Transition/transition';
 
 export function newTransactionFromEventSchema(
   eventSchema: EventSchema,
   partialTransition: Partial<Transition>
 ): Transition {
-  return {
+  return newTransition({
     events: partialTransition.events,
     target: partialTransition.target,
     source: partialTransition.source,
-    cond: eventSchema.cond || [],
-    actions: eventSchema.actions || [],
-  };
+    cond: eventSchema.cond,
+    actions: eventSchema.actions,
+  });
 }
